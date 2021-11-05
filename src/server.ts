c1a452856d8132d3b4846bb1dcc89e8cbccdda8e
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-05 13:43:58 -0300

    setting debug

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,15 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 const port = 3333
 
 app.get('/', (req, res) => res.json({ message: 'Hello dev' }))
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body
+  res.json({ name })
+})
 
 app.listen(port, () => console.log(`Server in: http://localhost:${port}`))
