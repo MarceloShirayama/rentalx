268235ea738496f30dba495c55b1f663e44224e4
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-11-06 16:29:36 -0300

    upload file with data stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,13 @@
+import fs from 'fs'
+import csvParse from 'csv-parse'
 export class ImportCategoryUseCase {
   execute (file: any): void {
-    console.log(file)
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = csvParse()
+
+    stream.pipe(parseFile)
+
+    parseFile.on('data', async (line) => console.log(line))
   }
 }
